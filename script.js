6a1bb9551f66ba91ebe19406b6b70b7c419425e4
GitHub User Name <email>
2020-11-03 10:26:29 -0700

    map added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 document.querySelector()
+
+function initMap() {
+    const lakeLouise = { lat: 51.425, lng: -116.1773 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: lakeLouise,
+    });
+        const marker = new google.maps.Marker({
+            position: lakeLouise,
+            map: map,
+    });
+}
