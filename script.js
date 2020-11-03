fbb6b3df2855427240e6c9b3bbca310868fb7bc0
Ilyas Sadybekov <ilyassadybekov@MacBook-Pro.local>
2020-11-03 12:13:15 -0700

    IS adding object

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.querySelector()
+
 
 function initMap() {
     const lakeLouise = { lat: 51.425, lng: -116.1773 };
     const lakeLouiseParking = { lat: 51.417123, lng: -116.215194 };
-    const morainLakeParking = { lat: 51.3294, lng: -116.1817 };
+    const moraineLakeParking = { lat: 51.3294, lng: -116.1817 };
     const overFlowParking = { lat: 51.3867448, lng: -116.1302803 };
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 10.5,
@@ -13,8 +13,8 @@ function initMap() {
     //     position: lakeLouise,
     //     map: map,
     // });
-    const markerMorainLakeParking = new google.maps.Marker({
-        position: morainLakeParking,
+    const markerMoraineLakeParking = new google.maps.Marker({
+        position: moraineLakeParking,
         map: map,
     }); 
     const markerLakeLouiseParking = new google.maps.Marker({
@@ -25,6 +25,55 @@ function initMap() {
         position: overFlowParking,
         map: map,
     });
-
-
 }
+
+
+const parkingLakeLouise = {
+    name: "Lake Louise Parking We did it!",
+    stalls: 200,
+    hours: {
+        monday: "8:00 - 22:00",
+        tuesday: "8:00 - 22:00",
+        wednesday: "8:00 - 22:00",
+        thursday: "8:00 - 22:00",
+        friday: "8:00 - 22:00",
+        saturday: "8:00 - 22:00",
+        sunday: "8:00 - 22:00",
+    },
+    responsible: "007",
+}
+
+const parkingMoraineLake = {
+    name: "Moraine Lake Parking",
+    stallsTotal: 100,
+    stallsAvailable: 0,
+    hours: {
+        monday: "8:00 - 22:00",
+        tuesday: "8:00 - 22:00",
+        wednesday: "8:00 - 22:00",
+        thursday: "8:00 - 22:00",
+        friday: "8:00 - 22:00",
+        saturday: "8:00 - 22:00",
+        sunday: "8:00 - 22:00",
+    },
+    responsible: "001",
+}
+
+const parkingOverFlow = {
+    name: "Over Flow Parking",
+    stalls: 400,
+    hours: {
+        monday: "8:00 - 22:00",
+        tuesday: "8:00 - 22:00",
+        wednesday: "8:00 - 22:00",
+        thursday: "8:00 - 22:00",
+        friday: "8:00 - 22:00",
+        saturday: "8:00 - 22:00",
+        sunday: "8:00 - 22:00",
+    },
+    responsible: "002",
+}
+
+const parkings = [parkingLakeLouise, parkingMoraineLake, parkingOverFlow]
+
+document.querySelector("#ParkingOneName").innerHTML = parkings[0].name
